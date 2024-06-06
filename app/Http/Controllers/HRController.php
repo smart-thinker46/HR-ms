ddac5c37287218292ef0d6bdb1295e9856cf9258
Soeng Souy <soengsouy.khmer09@gmail.com>
2024-06-06 18:11:08 +0700

    update folder HR

--- app/Http/Controllers/HRController.php
+++ app/Http/Controllers/HRController.php
@@ -9,54 +9,54 @@ class HRController extends Controller
     /** employee list */
     public function employeeList()
     {
-        return view('hr.employee');
+        return view('HR.employee');
     }
 
     /** holiday Page */
     public function holidayPage()
     {
-        return view('hr.holidays');
+        return view('HR.holidays');
     }
 
     /** leave Employee */
     public function leaveEmployee()
     {
-        return view('hr.leave-employee');
+        return view('HR.LeavesManage.leave-employee');
     }
 
     /** create Leave Employee */
     public function createLeaveEmployee()
     {
-        return view('hr.create-leave-employee');
+        return view('HR.LeavesManage.create-leave-employee');
     }
 
     /** leave HR */
     public function leaveHR() {
-        return view('hr.leave-hr');
+        return view('HR.LeavesManage.leave-hr');
     }
 
     /** attendance */
     public function attendance()
     {
-        return view('hr.attendance');
+        return view('HR.Attendance.attendance');
     }
 
     /** create Leave HR */
     public function createLeaveHR()
     {
-        return view('hr.create-leave-hr');
+        return view('HR.LeavesManage.create-leave-hr');
     }
 
     /** attendance Main */
     public function attendanceMain()
     {
-        return view('hr.attendance-main');
+        return view('HR.Attendance.attendance-main');
     }
 
     /** department */
     public function department()
     {
-        return view('hr.department');
+        return view('HR.department');
     }
 
 }
