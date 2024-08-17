7af5223c54bf7913fd8658f51afe374f3cc4699f
Soeng Souy <soengsouy.khmer09@gmail.com>
2024-08-17 17:09:55 +0700

    update leave auto

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,23 +60,16 @@ class User extends Authenticatable
         ];
     }
 
+    /** auto generate id */
     protected static function boot()
     {
         parent::boot();
         self::creating(function ($model) {
-            $getUser = self::orderBy('user_id', 'desc')->first();
-
-            if ($getUser) {
-                $latestID = intval(substr($getUser->user_id, 4));
-                $nextID = $latestID + 1;
-            } else {
-                $nextID = 1;
-            }
-            $model->user_id = 'KH_' . sprintf("%04s", $nextID);
-            while (self::where('user_id', $model->user_id)->exists()) {
-                $nextID++;
-                $model->user_id = 'KH_' . sprintf("%04s", $nextID);
-            }
+            $lastUser = self::orderBy('user_id', 'desc')->first();
+            $nextID  = $lastUser ? intval(substr($lastUser->user_id, 4)) + 1 : 1;
+            do {
+                $model->user_id = 'KH_' . sprintf("%04s", $nextID++);
+            } while (self::where('user_id', $model->user_id)->exists());
         });
     }
 
