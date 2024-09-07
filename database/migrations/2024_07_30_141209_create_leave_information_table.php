1863621b760ccb9fdf32e24b36cf1f3c2b99436a
Soeng Souy <soengsouy.khmer09@gmail.com>
2024-09-07 16:56:15 +0700

    update leave

--- database/migrations/2024_07_30_141209_create_leave_information_table.php
+++ database/migrations/2024_07_30_141209_create_leave_information_table.php
@@ -20,12 +20,13 @@ return new class extends Migration
         });
 
         DB::table('leave_information')->insert([
-            ['leave_type' => 'Medical Leave'  ,'leave_days' => '04','year_leave' => date('Y')],
-            ['leave_type' => 'Casual Leave'   ,'leave_days' => '08','year_leave' => date('Y')],
-            ['leave_type' => 'Sick Leave'     ,'leave_days' => '05','year_leave' => date('Y')],
-            ['leave_type' => 'Annual Leave'   ,'leave_days' => '12','year_leave' => date('Y')],
-            ['leave_type' => 'Use Leave'      ,'leave_days' => '09','year_leave' => date('Y')],
-            ['leave_type' => 'Remaining Leave','leave_days' => '18','year_leave' => date('Y')],
+            ['leave_type' => 'Medical Leave'  ,'leave_days'     => '04','year_leave' => date('Y')],
+            ['leave_type' => 'Casual Leave'   ,'leave_days'     => '08','year_leave' => date('Y')],
+            ['leave_type' => 'Sick Leave'     ,'leave_days'     => '05','year_leave' => date('Y')],
+            ['leave_type' => 'Annual Leave'   ,'leave_days'     => '12','year_leave' => date('Y')],
+            ['leave_type' => 'Use Leave'      ,'leave_days'     => '09','year_leave' => date('Y')],
+            ['leave_type' => 'Remaining Leave','leave_days'     => '18','year_leave' => date('Y')],
+            ['leave_type' => 'Total Leave Balance','leave_days' => '00','year_leave' => date('Y')]
         ]);
     }
 
